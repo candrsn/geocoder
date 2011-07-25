fc9126504d8e3013684cd5cb72299fe90cbf634c
Carl Anderson <carl.anderson@vadose.org>
2011-07-24 22:23:09 -0400

    Adjust cli.rb demos for use as a testing module

--- demos/cli.rb
+++ demos/cli.rb
@@ -1,7 +1,10 @@
+require 'rubygems'
 require 'geocoder/us/database'
 require 'pp'
 
-db = Geocoder::US::Database.new("/mnt/tiger2010/geocoder.db", :debug=>true)
+gdb="/opt/geocoder_us/geocoder.db"
+
+db = Geocoder::US::Database.new(gdb, :debug=>true)
 result = db.geocode(ARGV[0])
-pp(result)
-print "#{result[0][:lat]} N, #{-result[0][:lon]} W\n"
+#pp(result)
+print "#{result[0][:lat]} N, #{-result[0][:lon]} W, score: #{result[0][:score]}, precision: #{result[0][:precision]}\n"
