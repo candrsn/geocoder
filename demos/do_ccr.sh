105bec0feb53c0bb3a9c637bdd311cedb8b63ae4
Carl Anderson <carl.anderso@vadose.org>
2012-09-18 16:41:36 -0400

    Typos

--- demos/do_ccr.sh
+++ demos/do_ccr.sh
@@ -2,7 +2,7 @@
 
 # download the Central Contractor registry (CCR) from data.gov
 # place it in /opt/data/ccr/CCR.csv
-# build a tiger geocoder into /opy/geocoder_us/geocoder.db
+# build a tiger geocoder into /opt/geocoder_us/geocoder.db
 
 
 ruby -rubygems -I../lib do_ccr.rb
